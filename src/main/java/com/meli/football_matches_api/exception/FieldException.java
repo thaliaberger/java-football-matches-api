ee78c7e46e666f44e870f824377d457c0701a787
thaliaberger <berger.thalia@gmail.com>
2025-01-06 22:11:33 -0300

    create field exception

---
+++ src/main/java/com/meli/football_matches_api/exception/FieldException.java
@@ -0,0 +1,7 @@
+package com.meli.football_matches_api.exception;
+
+public class FieldException extends RuntimeException {
+    public FieldException(String message) {
+        super(message);
+    }
+}
