61791b1fb1c5920dbaa8f2e1cd0566f78737eb3c
thaliaberger <berger.thalia@gmail.com>
2025-01-07 13:59:18 -0300

    create update service

--- src/main/java/com/meli/football_matches_api/service/TeamService.java
+++ src/main/java/com/meli/football_matches_api/service/TeamService.java
@@ -3,6 +3,7 @@ package com.meli.football_matches_api.service;
 import com.meli.football_matches_api.DTO.TeamDTO;
 import com.meli.football_matches_api.exception.ConflictException;
 import com.meli.football_matches_api.exception.FieldException;
+import com.meli.football_matches_api.exception.NotFoundException;
 import com.meli.football_matches_api.model.Team;
 import com.meli.football_matches_api.repository.ITeam;
 import com.meli.football_matches_api.utils.Utils;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TeamService {
@@ -30,6 +33,17 @@ public class TeamService {
         return ResponseEntity.status(201).body(savedTeam);
     };
 
+    public ResponseEntity<TeamDTO> update(TeamDTO teamDTO) {
+        repository.findById(teamDTO.getId()).orElseThrow(() -> new NotFoundException("Team not found"));
+
+        validateFields(teamDTO);
+        validateIfTeamAlreadyExists(teamDTO.getName(), teamDTO.getState());
+
+        Team updatedTeam = new Team(teamDTO);
+        TeamDTO savedTeam = new TeamDTO(repository.save(updatedTeam));
+        return ResponseEntity.status(200).body(savedTeam);
+    }
+
     private void validateFields(TeamDTO teamDTO) {
         if (teamDTO.getName() == null || teamDTO.getName().isEmpty()) throw new FieldException("Field name cannot be empty");
         if (teamDTO.getIsActive() == null) throw new FieldException("Field isActive cannot be null");
@@ -49,7 +63,7 @@ public class TeamService {
 
         if (state.length() != 2) throw new FieldException("Field state must contain 2 characters");
 
-        if (!Utils.validateState(state)) throw new FieldException("state is not a valid");
+        if (!Utils.isValidState(state)) throw new FieldException("state is not a valid");
     };
 
     private void validateIfTeamAlreadyExists(String teamName, String state) {
