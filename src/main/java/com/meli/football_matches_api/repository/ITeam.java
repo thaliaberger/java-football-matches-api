b10757b0224c0d85f7220567a5c6bf0365358b9e
thaliaberger <berger.thalia@gmail.com>
2025-01-06 21:29:06 -0300

    create repository

---
+++ src/main/java/com/meli/football_matches_api/repository/ITeam.java
@@ -0,0 +1,7 @@
+package com.meli.football_matches_api.repository;
+
+import com.meli.football_matches_api.model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITeam extends JpaRepository<Team, Integer> {
+}
