56ff505775872109179e273f8314a2415daa013e
thaliaberger <berger.thalia@gmail.com>
2025-01-06 22:11:13 -0300

    create error handler

---
+++ src/main/java/com/meli/football_matches_api/handler/ErrorHandler.java
@@ -0,0 +1,23 @@
+package com.meli.football_matches_api.handler;
+
+import com.meli.football_matches_api.DTO.ErrorDTO;
+import com.meli.football_matches_api.exception.FieldException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(FieldException.class)
+    public ErrorDTO handler(FieldException ex) {
+        return new ErrorDTO(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value()
+        );
+    }
+}
