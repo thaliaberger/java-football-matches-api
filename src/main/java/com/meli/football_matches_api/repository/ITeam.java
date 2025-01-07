2d975e837b07d1531cdbd367cf47798f2caeacd8
thaliaberger <berger.thalia@gmail.com>
2025-01-07 14:23:59 -0300

    create List methods

--- src/main/java/com/meli/football_matches_api/repository/ITeam.java
+++ src/main/java/com/meli/football_matches_api/repository/ITeam.java
@@ -3,9 +3,17 @@ package com.meli.football_matches_api.repository;
 import com.meli.football_matches_api.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ITeam extends JpaRepository<Team, Integer> {
 
     Team findById(int id);
 
     Team findByNameAndState(String name, String state);
+
+    List<Team> findAllByState(String state);
+
+    List<Team> findAllByName(String name);
+
+    List<Team> findAllByIsActive(boolean isActive);
 }
