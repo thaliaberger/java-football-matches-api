2b6b1eda48c45aeb334a1749983aa9653872ac7d
thaliaberger <berger.thalia@gmail.com>
2025-01-07 14:02:14 -0300

    rename method

--- src/main/java/com/meli/football_matches_api/utils/Utils.java
+++ src/main/java/com/meli/football_matches_api/utils/Utils.java
@@ -6,7 +6,7 @@ public class Utils {
 
     private final static String[] STATES = { "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "TO" };
 
-    public static boolean validateState(String state) {
+    public static boolean isValidState(String state) {
 
         return Arrays.asList(STATES).contains(state);
     }
