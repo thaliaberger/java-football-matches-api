04e7912c315e78c23a7e9e9da9855bed760c8031
thaliaberger <berger.thalia@gmail.com>
2025-01-07 09:20:52 -0300

    create findByNameAndState

--- src/main/java/com/meli/football_matches_api/repository/ITeam.java
+++ src/main/java/com/meli/football_matches_api/repository/ITeam.java
@@ -4,4 +4,6 @@ import com.meli.football_matches_api.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITeam extends JpaRepository<Team, Integer> {
+
+    Team findByNameAndState(String name, String state);
 }
