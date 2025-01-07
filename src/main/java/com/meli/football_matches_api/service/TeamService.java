f3f48e3abc30626c045fc5b2886df7695593834f
thaliaberger <berger.thalia@gmail.com>
2025-01-06 22:13:34 -0300

    create validateFields method

--- src/main/java/com/meli/football_matches_api/service/TeamService.java
+++ src/main/java/com/meli/football_matches_api/service/TeamService.java
@@ -1,9 +1,13 @@
 package com.meli.football_matches_api.service;
 
 import com.meli.football_matches_api.DTO.TeamDTO;
+import com.meli.football_matches_api.exception.FieldException;
 import com.meli.football_matches_api.model.Team;
 import com.meli.football_matches_api.repository.ITeam;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class TeamService {
@@ -14,8 +18,24 @@ public class TeamService {
         this.repository = repository;
     };
 
-    public void create(TeamDTO teamDTO) {
+    public ResponseEntity<TeamDTO> create(TeamDTO teamDTO) {
+        validateFields(teamDTO);
         Team newTeam = new Team(teamDTO);
-        repository.save(newTeam);
+        TeamDTO savedTeam = new TeamDTO(repository.save(newTeam));
+        return ResponseEntity.status(201).body(savedTeam);
     };
+
+    private void validateFields(TeamDTO teamDTO) {
+        validateDateCreated(teamDTO.getDateCreated());
+    }
+
+    private void validateDateCreated(LocalDate date) {
+        if (date == null) {
+            throw new FieldException("dateCreated cannot be null");
+        }
+
+        if (date.isAfter(LocalDate.now())) {
+            throw new FieldException("dateCreated cannot be in the future");
+        }
+    }
 }
