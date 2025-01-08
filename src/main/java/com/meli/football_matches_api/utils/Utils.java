d09a52dac095573047df083be13ebd55ff1885f1
thaliaberger <berger.thalia@gmail.com>
2025-01-08 16:00:29 -0300

    create handleSortParams

--- src/main/java/com/meli/football_matches_api/utils/Utils.java
+++ src/main/java/com/meli/football_matches_api/utils/Utils.java
@@ -2,6 +2,8 @@ package com.meli.football_matches_api.utils;
 
 import com.meli.football_matches_api.DTO.TeamDTO;
 import com.meli.football_matches_api.model.Team;
+import org.springframework.data.domain.Sort;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,5 +18,10 @@ public class Utils {
         }
 
         return teamDTOs;
+    }
+
+    public static Sort handleSortParams(String sort) {
+        String[] sortParams = sort.split(",");
+        return sortParams[1].equalsIgnoreCase("asc") ? Sort.by(sortParams[0]).ascending() : Sort.by(sortParams[0]).descending();
     }
 }
