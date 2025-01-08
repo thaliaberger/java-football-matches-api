e8534abea171f538a7fd8e4ecd787ddf08be7eb1
thaliaberger <berger.thalia@gmail.com>
2025-01-08 17:12:00 -0300

    call match validations

--- src/main/java/com/meli/football_matches_api/service/MatchService.java
+++ src/main/java/com/meli/football_matches_api/service/MatchService.java
@@ -3,6 +3,7 @@ package com.meli.football_matches_api.service;
 import com.meli.football_matches_api.DTO.MatchDTO;
 import com.meli.football_matches_api.model.Match;
 import com.meli.football_matches_api.repository.IMatch;
+import com.meli.football_matches_api.validations.MatchValidations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class MatchService {
     };
 
     public ResponseEntity<MatchDTO> create(MatchDTO matchDTO) {
+        MatchValidations.validateFields(matchDTO);
         Match newMatch = new Match(matchDTO);
         MatchDTO savedMatch = new MatchDTO(repository.save(newMatch));
         return ResponseEntity.status(201).body(savedMatch);
