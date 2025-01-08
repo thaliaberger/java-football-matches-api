a88dd1b3f281d14ebc02523615d8bf834c612b96
thaliaberger <berger.thalia@gmail.com>
2025-01-08 16:10:33 -0300

    create match service

---
+++ src/main/java/com/meli/football_matches_api/service/MatchService.java
@@ -0,0 +1,29 @@
+package com.meli.football_matches_api.service;
+
+import com.meli.football_matches_api.DTO.MatchDTO;
+import com.meli.football_matches_api.model.Match;
+import com.meli.football_matches_api.repository.IMatch;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MatchService {
+
+    private final IMatch repository;
+
+    public MatchService(IMatch repository) {
+        this.repository = repository;
+    };
+
+    public ResponseEntity<MatchDTO> create(MatchDTO matchDTO) {
+        Match newMatch = new Match(matchDTO);
+        MatchDTO savedMatch = new MatchDTO(repository.save(newMatch));
+        return ResponseEntity.status(201).body(savedMatch);
+    };
+
+    public List<Match> list() {
+        return repository.findAll();
+    }
+}
