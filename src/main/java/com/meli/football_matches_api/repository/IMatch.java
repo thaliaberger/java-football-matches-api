16de55b35af865d29da7fb32d34b72ec1b86adef
thaliaberger <berger.thalia@gmail.com>
2025-01-08 16:09:49 -0300

    remove empty line

--- src/main/java/com/meli/football_matches_api/repository/IMatch.java
+++ src/main/java/com/meli/football_matches_api/repository/IMatch.java
@@ -3,7 +3,6 @@ package com.meli.football_matches_api.repository;
 import com.meli.football_matches_api.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface IMatch extends JpaRepository<Match, Integer> {
 
 }
