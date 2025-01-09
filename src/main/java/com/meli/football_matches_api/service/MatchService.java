514b5ef60ea0b1816830ecc3da7a2a12b516c6f0
thaliaberger <berger.thalia@gmail.com>
2025-01-08 21:11:15 -0300

    teamRepository injection

--- src/main/java/com/meli/football_matches_api/service/MatchService.java
+++ src/main/java/com/meli/football_matches_api/service/MatchService.java
@@ -3,7 +3,9 @@ package com.meli.football_matches_api.service;
 import com.meli.football_matches_api.DTO.MatchDTO;
 import com.meli.football_matches_api.model.Match;
 import com.meli.football_matches_api.repository.IMatch;
+import com.meli.football_matches_api.repository.ITeam;
 import com.meli.football_matches_api.validations.MatchValidations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,15 @@ public class MatchService {
 
     private final IMatch repository;
 
+    @Autowired
+    private ITeam teamRepository;
+
     public MatchService(IMatch repository) {
         this.repository = repository;
     };
 
     public ResponseEntity<MatchDTO> create(MatchDTO matchDTO) {
-        MatchValidations.validateFields(matchDTO);
+        MatchValidations.validateFields(matchDTO, teamRepository);
         Match newMatch = new Match(matchDTO);
         MatchDTO savedMatch = new MatchDTO(repository.save(newMatch));
         return ResponseEntity.status(201).body(savedMatch);
