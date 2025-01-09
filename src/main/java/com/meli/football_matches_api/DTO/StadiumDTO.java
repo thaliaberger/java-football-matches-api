03036ee09eb36daf3ae825041fe17622a9e39625
thaliaberger <berger.thalia@gmail.com>
2025-01-09 14:11:48 -0300

    create stadiumDTO

---
+++ src/main/java/com/meli/football_matches_api/DTO/StadiumDTO.java
@@ -0,0 +1,33 @@
+package com.meli.football_matches_api.DTO;
+
+import com.meli.football_matches_api.model.Stadium;
+import org.springframework.beans.BeanUtils;
+
+public class StadiumDTO {
+
+    private Long id;
+
+    private String name;
+
+    public StadiumDTO() {}
+
+    public StadiumDTO(Stadium stadium) {
+        BeanUtils.copyProperties(stadium, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
