c9205e8036149ce479bce5f5c8b7a11964767ea9
thaliaberger <berger.thalia@gmail.com>
2025-01-09 16:58:55 -0300

    handle match score

--- src/main/java/com/meli/football_matches_api/service/MatchService.java
+++ src/main/java/com/meli/football_matches_api/service/MatchService.java
@@ -3,6 +3,7 @@ package com.meli.football_matches_api.service;
 import com.meli.football_matches_api.DTO.MatchDTO;
 import com.meli.football_matches_api.exception.NotFoundException;
 import com.meli.football_matches_api.model.Match;
+import com.meli.football_matches_api.model.Team;
 import com.meli.football_matches_api.repository.MatchRepository;
 import com.meli.football_matches_api.repository.TeamRepository;
 import com.meli.football_matches_api.utils.Utils;
@@ -38,8 +39,12 @@ public class MatchService {
 
     private ResponseEntity<MatchDTO> createOrUpdate(MatchDTO matchDTO, Boolean isUpdate) {
         MatchValidations.validateFields(matchDTO, matchRepository, teamRepository);
+
         Match newMatch = new Match(matchDTO);
         MatchDTO savedMatch = new MatchDTO(matchRepository.save(newMatch));
+
+        handleScoreAndUpdateTeams(matchDTO.getIdHomeTeam(), matchDTO.getHomeGoals(), matchDTO.getIdAwayTeam(), matchDTO.getAwayGoals());
+
         int statusCode = isUpdate ? 200 : 201;
         return ResponseEntity.status(statusCode).body(savedMatch);
     }
@@ -73,4 +78,32 @@ public class MatchService {
         return ResponseEntity.status(200).body(Utils.convertToMatchDTO(matches));
     }
 
+    private void handleScoreAndUpdateTeams(Long homeTeamId, Integer homeTeamGoals, Long awayTeamId, Integer awayTeamGoals) {
+        Team homeTeam = teamRepository.findById(homeTeamId.intValue());
+        Team awayTeam = teamRepository.findById(awayTeamId.intValue());
+
+        if (homeTeamGoals > 0) {
+            homeTeam.setScoredGoals(homeTeam.getScoredGoals() + homeTeamGoals);
+            awayTeam.setConcededGoals(awayTeam.getConcededGoals() + awayTeamGoals);
+        }
+
+        if (awayTeamGoals > 0) {
+            awayTeam.setScoredGoals(awayTeam.getScoredGoals() + awayTeamGoals);
+            homeTeam.setConcededGoals(homeTeam.getConcededGoals() + homeTeamGoals);
+        }
+
+        if (homeTeamGoals > awayTeamGoals) {
+            homeTeam.setWins(homeTeam.getWins() + 1);
+            awayTeam.setLosses(awayTeam.getLosses() + 1);
+        } else if (awayTeamGoals > homeTeamGoals) {
+            awayTeam.setWins(awayTeam.getWins() + 1);
+            homeTeam.setLosses(homeTeam.getLosses() + 1);
+        } else {
+            homeTeam.setDraws(homeTeam.getDraws() + 1);
+            awayTeam.setDraws(awayTeam.getDraws() + 1);
+        }
+
+        teamRepository.save(awayTeam);
+        teamRepository.save(homeTeam);
+    }
 }
