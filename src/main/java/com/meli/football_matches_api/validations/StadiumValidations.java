ccb0f83219a825cd055de116bdb9d9c794d22dcc
thaliaberger <berger.thalia@gmail.com>
2025-01-09 14:11:31 -0300

    create stadium name validations

---
+++ src/main/java/com/meli/football_matches_api/validations/StadiumValidations.java
@@ -0,0 +1,16 @@
+package com.meli.football_matches_api.validations;
+
+import com.meli.football_matches_api.exception.ConflictException;
+import com.meli.football_matches_api.exception.FieldException;
+import com.meli.football_matches_api.model.Stadium;
+import com.meli.football_matches_api.repository.StadiumRepository;
+
+public class StadiumValidations {
+
+    public static void validateName(String name, StadiumRepository stadiumRepository) {
+        if (name == null || name.isEmpty()) throw new FieldException("Stadium name cannot be null or empty");
+        if (name.length() < 3) throw new FieldException("Stadium name must be at least 3 characters");
+        Stadium existingStadium = stadiumRepository.findByName(name);
+        if (existingStadium != null) throw new ConflictException("Stadium with name [" + name + "] already exists");
+    }
+}
