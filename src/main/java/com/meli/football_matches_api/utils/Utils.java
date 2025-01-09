319003c15c0a3edf02c1d3520ef1b260aa00cd06
thaliaberger <berger.thalia@gmail.com>
2025-01-09 13:07:08 -0300

    create convertToMatchDTO

--- src/main/java/com/meli/football_matches_api/utils/Utils.java
+++ src/main/java/com/meli/football_matches_api/utils/Utils.java
@@ -1,6 +1,8 @@
 package com.meli.football_matches_api.utils;
 
+import com.meli.football_matches_api.DTO.MatchDTO;
 import com.meli.football_matches_api.DTO.TeamDTO;
+import com.meli.football_matches_api.model.Match;
 import com.meli.football_matches_api.model.Team;
 import org.springframework.data.domain.Sort;
 
@@ -23,5 +25,16 @@ public class Utils {
     public static Sort handleSortParams(String sort) {
         String[] sortParams = sort.split(",");
         return sortParams[1].equalsIgnoreCase("asc") ? Sort.by(sortParams[0]).ascending() : Sort.by(sortParams[0]).descending();
+    }
+
+    public static List<MatchDTO> convertToMatchDTO(List<Match> matches) {
+        List<MatchDTO> matchDTOS = new ArrayList<>();
+
+        for (Match match : matches) {
+            MatchDTO matchDTO = new MatchDTO(match);
+            matchDTOS.add(matchDTO);
+        }
+
+        return matchDTOS;
     }
 }
