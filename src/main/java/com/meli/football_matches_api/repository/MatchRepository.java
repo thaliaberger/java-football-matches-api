74d669e734de9b067364f14320043f8764f8ff0d
thaliaberger <berger.thalia@gmail.com>
2025-01-10 16:37:35 -0300

    add repository method

--- src/main/java/com/meli/football_matches_api/repository/MatchRepository.java
+++ src/main/java/com/meli/football_matches_api/repository/MatchRepository.java
@@ -1,6 +1,7 @@
 package com.meli.football_matches_api.repository;
 
 import com.meli.football_matches_api.model.Match;
+import com.meli.football_matches_api.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ public interface MatchRepository extends JpaRepository<Match, Integer> {
 
     Match findById(Long id);
 
-    List<Match> findAllByIdAwayTeam(Long idAwayTeam);
+    List<Match> findAllByAwayTeam(Team awayTeam);
+
+    List<Match> findAllByHomeTeam(Team homeTeam);
 }
