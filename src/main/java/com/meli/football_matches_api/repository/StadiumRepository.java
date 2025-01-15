1039e1b0540f00e36ece6dc5f0fc2bfcad1a9dd4
thaliaberger <berger.thalia@gmail.com>
2025-01-15 13:53:19 -0300

    add existsByName method

--- src/main/java/com/meli/football_matches_api/repository/StadiumRepository.java
+++ src/main/java/com/meli/football_matches_api/repository/StadiumRepository.java
@@ -4,7 +4,7 @@ import com.meli.football_matches_api.model.Stadium;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StadiumRepository extends JpaRepository<Stadium, Integer> {
-    Stadium findByName(String name);
-
     Stadium findById(Long id);
+
+    Boolean existsByName(String name);
 }
--- src/main/java/com/meli/football_matches_api/validations/StadiumValidations.java
+++ src/main/java/com/meli/football_matches_api/validations/StadiumValidations.java
@@ -3,7 +3,6 @@ package com.meli.football_matches_api.validations;
 import com.meli.football_matches_api.exception.ConflictException;
 import com.meli.football_matches_api.exception.FieldException;
 import com.meli.football_matches_api.exception.NotFoundException;
-import com.meli.football_matches_api.model.Stadium;
 import com.meli.football_matches_api.repository.StadiumRepository;
 
 public class StadiumValidations {
@@ -11,8 +10,7 @@ public class StadiumValidations {
     public static void validateName(String name, StadiumRepository stadiumRepository) {
         if (name == null || name.isEmpty()) throw new FieldException("Stadium name cannot be null or empty");
         if (name.length() < 3) throw new FieldException("Stadium name must be at least 3 characters");
-        Stadium existingStadium = stadiumRepository.findByName(name);
-        if (existingStadium != null) throw new ConflictException("Stadium with name [" + name + "] already exists");
+        if (stadiumRepository.existsByName(name)) throw new ConflictException("Stadium with name [" + name + "] already exists");
     }
 
     public static void validateIfStadiumExists(int stadiumId, StadiumRepository stadiumRepository) {
