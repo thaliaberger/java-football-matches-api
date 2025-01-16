d4534561b614a86729d8b565520690785de44ecf
thaliaberger <berger.thalia@gmail.com>
2025-01-16 09:55:32 -0300

    test match create case

--- src/test/java/com/meli/football_matches_api/service/MatchServiceTest.java
+++ src/test/java/com/meli/football_matches_api/service/MatchServiceTest.java
@@ -1,19 +1,23 @@
 package com.meli.football_matches_api.service;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import com.meli.football_matches_api.DTO.MatchDTO;
-import com.meli.football_matches_api.DTO.StadiumDTO;
 import com.meli.football_matches_api.model.Match;
 import com.meli.football_matches_api.model.Stadium;
 import com.meli.football_matches_api.model.Team;
 import com.meli.football_matches_api.repository.MatchRepository;
 import com.meli.football_matches_api.repository.StadiumRepository;
+import com.meli.football_matches_api.repository.TeamRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
@@ -21,20 +25,52 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-
+@ExtendWith(MockitoExtension.class)
 class MatchServiceTest {
 
     @Mock
     private MatchRepository matchRepository;
 
+    @Mock
+    private StadiumRepository stadiumRepository;
+
+    @Mock
+    private TeamRepository teamRepository;
+
     @InjectMocks
     private MatchService matchService;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
+    void setUp() {}
+
+    @Test
+    @DisplayName("Should create Match successfully")
+    void createCaseSuccess() {
+        Team team1 = new Team(1L, "Flamengo", "RJ", LocalDate.of(1980, 1, 1), true);
+        Team team2 = new Team(2L, "Fluminense", "RJ", LocalDate.of(1980, 1, 1), true);
+        Stadium stadium = new Stadium(1L, "Morumbi", null, null);
+        List<Match> matches = new ArrayList<>();
+
+        Match match1 = new Match(1L, 1, 0, LocalDateTime.of(2024, 1, 2, 10, 10, 10), team1, team2, stadium);
+        Match match2 = new Match(2L, 0, 0, LocalDateTime.of(2023, 1, 3, 10, 10, 10), team1, team2, stadium);
+        matches.add(match1);
+        matches.add(match2);
+        stadium.setMatches(matches);
+
+        long matchId = 3L;
+        Match newMatch = new Match(matchId, 0, 0, LocalDateTime.of(2023, 1, 6, 10, 10, 10), team1, team2, stadium);
+        MatchDTO matchDTO = new MatchDTO(newMatch);
+
+        when(teamRepository.findById(1L)).thenReturn(team1);
+        when(teamRepository.findById(2L)).thenReturn(team2);
+        when(stadiumRepository.findById(1L)).thenReturn(stadium);
+        when(matchRepository.save(any(Match.class))).thenReturn(new Match(matchDTO));
+
+        ResponseEntity<MatchDTO> response = matchService.create(matchDTO);
+
+        Assertions.assertEquals(201, response.getStatusCode().value());
+        Assertions.assertNotNull(response.getBody());
+        Assertions.assertEquals(matchId, response.getBody().getId());
     }
 
     @Test
