9bb1f6a347ebc0344ccbf703ce9e00ea0e58604e
thaliaberger <berger.thalia@gmail.com>
2025-01-24 13:23:07 -0300

    test team controller list

--- src/test/java/com/meli/football_matches_api/controller/TeamControllerTest.java
+++ src/test/java/com/meli/football_matches_api/controller/TeamControllerTest.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.meli.football_matches_api.DTO.TeamDTO;
+import com.meli.football_matches_api.model.Match;
+import com.meli.football_matches_api.model.Stadium;
 import com.meli.football_matches_api.model.Team;
 import com.meli.football_matches_api.service.TeamService;
+import com.meli.football_matches_api.utils.Utils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -35,7 +41,12 @@ class TeamControllerTest {
 
     long teamId = 1L;
     Team team1 = new Team();
+    Team team2 = new Team();
+    Team team3 = new Team();
     TeamDTO teamDTO = new TeamDTO();
+    List<Match> team1HomeMatches = new ArrayList<>();
+    List<Match> team1AwayMatches = new ArrayList<>();
+    List<Team> teams = new ArrayList<>();
     ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
@@ -44,7 +55,26 @@ class TeamControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(teamController).build();
 
         team1 = new Team(teamId, "Flamengo", "RJ", LocalDate.of(1980, 1, 1), true);
+        team2 = new Team(2L, "Fluminense", "RJ", LocalDate.of(1980, 1, 1), true);
+        team3 = new Team(3L, "Figueirense", "SC", LocalDate.of(1980, 1, 1), true);
         teamDTO = new TeamDTO(team1);
+
+        Stadium stadium = new Stadium(1L, "Morumbi", null, null);
+        Match match = new Match(1L, 4, 0, LocalDateTime.of(2000, 1, 2, 10, 10, 10), team1, team2, stadium);
+        Match match2 = new Match(2L, 0, 0, LocalDateTime.of(2000, 1, 3, 10, 10, 10), team1, team2, stadium);
+        Match match3 = new Match(3L, 2, 1, LocalDateTime.of(2000, 1, 3, 10, 10, 10), team3, team1, stadium);
+
+        team1HomeMatches.add(match);
+        team1HomeMatches.add(match2);
+        team1.setHomeMatches(team1HomeMatches);
+        team1AwayMatches.add(match3);
+        team1.setAwayMatches(team1AwayMatches);
+        team2.setAwayMatches(team1HomeMatches);
+        team3.setHomeMatches(team1AwayMatches);
+
+        teams.add(team3);
+        teams.add(team2);
+        teams.add(team1);
 
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
@@ -95,5 +125,17 @@ class TeamControllerTest {
                         .content(objectMapper.writeValueAsString(teamDTO)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(teamId));
+    }
+
+    @Test
+    @DisplayName("Should get list of Teams successfully")
+    void listCaseSuccess() throws Exception {
+        when(teamService.list(0, 1000, "id,asc", null, null, null)).thenReturn(Utils.convertToTeamDTO(teams));
+
+        mockMvc.perform(get("/team/list")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(teams)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(3));
     }
 }
