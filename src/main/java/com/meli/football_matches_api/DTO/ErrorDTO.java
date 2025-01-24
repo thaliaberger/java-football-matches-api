6406a1550fde073f16b510a7a4057b6f53f97f17
thaliaberger <berger.thalia@gmail.com>
2025-01-24 09:35:27 -0300

    create error handler

---
+++ src/main/java/com/meli/football_matches_api/DTO/ErrorDTO.java
@@ -0,0 +1,22 @@
+package com.meli.football_matches_api.DTO;
+
+public class ErrorDTO {
+    private String message;
+    private int status;
+    public ErrorDTO(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public int getStatus() {
+        return status;
+    }
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/meli/football_matches_api/handler/ErrorHandler.java
@@ -0,0 +1,41 @@
+package com.meli.football_matches_api.handler;
+import com.meli.football_matches_api.DTO.ErrorDTO;
+import com.meli.football_matches_api.exception.ConflictException;
+import com.meli.football_matches_api.exception.FieldException;
+import com.meli.football_matches_api.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ErrorHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(FieldException.class)
+    public ErrorDTO handler(FieldException ex) {
+        return new ErrorDTO(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value()
+        );
+    }
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody
+    @ExceptionHandler(ConflictException.class)
+    public ErrorDTO handler(ConflictException ex) {
+        return new ErrorDTO(
+                ex.getMessage(),
+                HttpStatus.CONFLICT.value()
+        );
+    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorDTO handler(NotFoundException ex) {
+        return new ErrorDTO(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        );
+    }
+}
